b164d87766af3a163da5ba6b9697923bdfbec473
ckcsb <ckcsb@126.com>
2019-01-28 19:58:34 +0800

    mediasoup sequence

--- app/src/main/java/com/versatica/mediasoup/demo/App.kt
+++ app/src/main/java/com/versatica/mediasoup/demo/App.kt
@@ -106,9 +106,14 @@ class App(val roomId: String, val peerName: String, val context: Context) {
         socket.on("mediasoup-notification") {
             val notification = it[0] as org.json.JSONObject
             logger.debug("New notification came from server: $notification")
-            roomObj.receiveNotification(notification).subscribe{
+            roomObj.receiveNotification(notification).subscribe(
+                {
 
-            }
+                },
+                {
+                    Toast.makeText(context,it.message,Toast.LENGTH_SHORT).show()
+                }
+            )
         }
     }
 
